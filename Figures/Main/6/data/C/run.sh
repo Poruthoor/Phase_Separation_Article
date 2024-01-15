de39f81881364dd1e641fc36ea556722f82da4ee
Poruthoor <ashlinjamesporuthoor@gmail.com>
2024-01-15 03:26:48 -0500

    Added missing C Figure Data

---
+++ Figures/Main/6/data/C/run.sh
@@ -0,0 +1,2 @@
+python3 ../multi_last.py --cv newpcoord --suffix DAPC_multi --xRange "(0, 1)" --yRange "(0, 5)" --xLabel FLC_opt --dat_files DAPC/{298,323,353,423}K/*average_*.dat
+python3 ../multi_last.py --cv newpcoord --suffix DIPC_multi --xRange "(0, 1)" --yRange "(0, 5)" --xLabel FLC_opt --dat_files DIPC/{298,323,353,423}K/*average_*.dat
